10669108bd751660a4a09549ebc1a5d3619ca828
Gegel85 <Gegel85@laposte.net>
2020-05-17 19:45:07 +0200

    Added .hpp

---
+++ include/osu_map_parser.hpp
@@ -0,0 +1,12 @@
+//
+// Created by Gegel85 on 17/05/2020.
+//
+
+#ifndef OSUREPLAY_PLAYER_OSU_MAP_PARSER_HPP
+#define OSUREPLAY_PLAYER_OSU_MAP_PARSER_HPP
+
+extern "C" {
+#include "osu_map_parser.h"
+};
+
+#endif //OSUREPLAY_PLAYER_OSU_MAP_PARSER_HPP
